f8735adb4d6c3070d63fe26c92e4e7e2a323ab4a
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-07-30 23:06:40 +0300

    add database connection

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "config.env" });
+
+// Connect to database
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.error(`database connection error: ${err}`);
+  });
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode is ${process.env.NODE_ENV}`);
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.status(200).send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("server started");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
