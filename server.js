67486dce591e9d5ffa50ee8005b8bf376711959b
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-07-31 18:28:14 +0300

    restructure server

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+
+const app = require("./app");
 const connectDB = require("./E-commerce/config/database");
 
 dotenv.config({ path: "config.env" });
 
-//create express app
-const app = express();
-
 // connect to database
 connectDB();
-
-// middleware
-app.use(express.json());
-
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-  console.log(`mode is ${process.env.NODE_ENV}`);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
