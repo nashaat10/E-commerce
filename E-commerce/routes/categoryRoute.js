0bfbdaa8b8b621ec716c7a7a608d88678f3fcabd
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-04 00:35:06 +0300

    add delete and get category routes

--- E-commerce/routes/categoryRoute.js
+++ E-commerce/routes/categoryRoute.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const categoryController = require("../controllers/categoryController");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .post(categoryController.createCategory)
+  .get(categoryController.getAllCategories);
+
+router.route("/:id").delete(categoryController.deleteCategory);
 
 module.exports = router;
