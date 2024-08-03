245f86dc477b6c963b4eb693065d669170fecf33
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-08-03 23:08:03 +0300

    add error handler middleware

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./E-commerce/utils/appError");
+const globalErrorHandler = require("./E-commerce/controllers/errorController");
 
 const app = express();
 
@@ -13,5 +15,12 @@ app.use(express.json());
 
 // Route
 app.use(express.json());
+
+// all is used to handle all http requests and responses from the server
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
